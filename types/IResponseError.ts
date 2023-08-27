115780a8fb7344b234686ef147e15cebb9ceee84
Alexander Ho <ho.alexander.g@gmail.com>
2023-08-27 17:32:32 +0800

    feat(type): add type guard for IResponseError

--- types/IResponseError.ts
+++ types/IResponseError.ts
@@ -15,5 +15,23 @@ interface IResponseError {
   errors: IError[];
 }
 
-export { CustomErrorCodes };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isIResponseError = (obj: any): obj is IResponseError => {
+  if (typeof obj === 'object' && 'errors' in obj) {
+    if (Array.isArray(obj.errors)) {
+      return obj.errors.every(
+        (error: IError) =>
+          typeof error.code === 'string' &&
+          typeof error.message === 'string' &&
+          (!error.path ||
+            (Array.isArray(error.path) &&
+              error.path.every((path) => typeof path === 'string')))
+      );
+    }
+  }
+
+  return false;
+};
+
+export { CustomErrorCodes, isIResponseError };
 export type { IError, IResponseError };
