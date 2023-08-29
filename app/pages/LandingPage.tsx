bbc899b796c6012e88515999653662424bedcdcf
Alexander Ho <ho.alexander.g@gmail.com>
2023-08-29 13:09:11 +0800

    refactor: seperate landing page from root page

---
+++ app/pages/LandingPage.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import MainNavbar from '@/app/components/MainNavbar';
+
+const LandingPage = () => {
+  return (
+    <>
+      <MainNavbar />
+
+      <div className="container mx-auto px-3 max-w-screen-xl">
+        <main className="mx-auto max-w-screen-md py-16 flex flex-col items-center text-center">
+          <h1 className="mb-6 text-6xl font-bold primary-content">
+            Organize your work and life with TaskHub.
+          </h1>
+          <p className="mb-6 mx-auto max-w-screen-sm text-lg">
+            An open source project that provides a powerful task management
+            solution for individuals and teams.
+          </p>
+
+          <Link href="/account/signup" className="btn btn-primary">
+            Get Started
+          </Link>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
