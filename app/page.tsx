f897b8952519127d06251efb3ee97489cd548be6
Alexander Ho <ho.alexander.g@gmail.com>
2023-08-29 13:55:46 +0800

    feat: add home page for authenticated user

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+'use client';
+import { useSession } from 'next-auth/react';
 import LandingPage from './pages/LandingPage';
+import HomePage from './pages/HomePage';
 
 const App = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return <HomePage />;
+  }
+
   return <LandingPage />;
 };
 
